430fdcf25f07ff934c014c940db2520276c8498c
hwlee0103 <sdiverHyeWon0103@gmail.com>
2021-05-06 19:56:45 +0900

    how to use jsx
    jsx is html in js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Potato from "./Potato";
 
 function App() {
-  return <div>Hello!!!22222</div>;
+  return (
+    <div>
+      <h1>Hello!!!22222</h1>
+      <Potato />
+    </div>
+  );
 }
 
 export default App;
