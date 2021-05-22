7f75dbe560d906f0dedcc4b87de2b24dddf6f0d1
hwlee0103 <sdiverHyeWon0103@gmail.com>
2021-05-22 17:10:11 +0900

    fix error
    function Movie() need {} : function Movie({})

--- src/Movie.js
+++ src/Movie.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Movie(id, year, title, summary, poster) {
+function Movie({ id, year, title, summary, poster }) {
   return <h5>{title}</h5>;
 }
 
